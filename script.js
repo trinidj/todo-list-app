14e0f73062898eced937358067171e1aad43eb79
trinidj <jm.trinidad199@gmail.com>
2025-07-10 14:04:17 -0400

    feat: To-Do list now reads from an array stored in local storage

--- script.js
+++ script.js
@@ -10,16 +10,36 @@ const dialog = document.getElementById('modern-dialog');
 dialogButtons.add.addEventListener('click', () => dialog.showModal());
 dialogButtons.cancel.addEventListener('click', () => dialog.close());
 
-// Tasks and Local Storage
-let tasks = [];
 
+
+// Tasks and Local Storage
+let tasks = JSON.parse(localStorage.getItem('todo-tasks')) || [];
 const taskInput = document.querySelector('input');
+const todoList = document.getElementById('todo-list');
+
+tasks.forEach(task => {
+  const li = document.createElement('li');
+  li.className = 'list-item';
+  li.textContent = task;
+  todoList.appendChild(li);
+});
 
 dialogButtons.submit.addEventListener('click', () => {
   try {
     tasks.push(taskInput.value);
-    localStorage.setItem('tasks', tasks);
-    
+    localStorage.setItem('todo-tasks', JSON.stringify(tasks));
+
+    console.log(tasks);
+
+    todoList.innerHTML = '';
+    tasks.forEach(task => {
+      const li = document.createElement('li');
+      li.className = 'list-item';
+      li.textContent = task;
+      todoList.appendChild(li);
+    });
+
+    taskInput.value = '';
     dialog.close();
   } catch (error) {
     console.error('Failed to add task:', error);
