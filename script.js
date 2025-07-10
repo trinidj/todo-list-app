d11dff7d1793d71a5032a2ec37ad484fa7353fb3
trinidj <jm.trinidad199@gmail.com>
2025-07-09 21:04:05 -0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const dialogButtons = {
   add: document.getElementById('add-task'),
   cancel: document.getElementById('cancel-task'),
+  submit: document.getElementById('submit-task'),
 };
+
+const dialog = document.getElementById('modern-dialog');
+const todoList = document.getElementById('todo-list');
+const taskInput = document.querySelectorAll('.task-input');
+
+let tasks = [];
 
 dialogButtons.add.addEventListener('click', () => document.getElementById('modern-dialog').showModal());
 dialogButtons.cancel.addEventListener('click', () => document.getElementById('modern-dialog').close());
